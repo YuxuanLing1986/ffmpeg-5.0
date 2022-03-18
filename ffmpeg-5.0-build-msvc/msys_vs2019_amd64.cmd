ea93a169d056eb227aa9b6d622b5aae08e89f1ed
yuxuan.ling <yuxuan.ling@mthreads.com>
2022-03-18 11:18:17 +0800

    update

--- ffmpeg-5.0-build-msvc/msys_vs2019_amd64.cmd
+++ ffmpeg-5.0-build-msvc/msys_vs2019_amd64.cmd
@@ -1,3 +1,3 @@
 set MSYS2_PATH_TYPE=inherit
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 msys2_shell.cmd -mingw64
